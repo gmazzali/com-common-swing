dc091d4df6541c555d2c30f2e4241cdddda29a9d
guillermomazzali <guillermomazzali@gmail.com>
2014-05-05 20:59:32 +0000

    Cambios en el framework de crud.

--- src/main/java/com/common/swing/domain/model/crud/filter/impl/FilterFormImpl.java
+++ src/main/java/com/common/swing/domain/model/crud/filter/impl/FilterFormImpl.java
@@ -1,12 +1,11 @@
 package com.common.swing.domain.model.crud.filter.impl;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 import javax.swing.JPanel;
 
-import org.apache.log4j.Logger;
-
-import com.common.swing.domain.model.crud.filter.EntityFilterForm;
+import com.common.swing.domain.model.crud.filter.FilterForm;
 import com.common.util.domain.model.Persistence;
 
 /**
@@ -21,7 +20,14 @@ import com.common.util.domain.model.Persistence;
  * @param <PK>
  *            La clase que utilizamos para identificar las entidades filtradas.
  */
-public abstract class EntityFilterPanel<E extends Persistence<PK>, PK extends Serializable> extends JPanel implements EntityFilterForm<E> {
+public abstract class FilterFormImpl<E extends Persistence<PK>, PK extends Serializable> extends JPanel implements FilterForm<E> {
 	private static final long serialVersionUID = 1L;
-	private static final Logger log = Logger.getLogger(EntityFilterPanel.class);
+
+	// private static final Logger log = Logger.getLogger(FilterFormImpl.class);
+
+	@Override
+	public Collection<E> filter() {
+		// TODO Hacer lo del filtrado.
+		return null;
+	}
 }
---
+++ src/main/java/com/common/swing/domain/model/crud/util/FormTypeEnum.java
@@ -0,0 +1,30 @@
+package com.common.swing.domain.model.crud.util;
+
+/**
+ * La enumeración que contiene los modos en los que puede ser llamado un formulario de edición de una entidad.
+ * 
+ * <pre>
+ * {@link FormTypeEnum#NEW}
+ * {@link FormTypeEnum#EDIT}
+ * {@link FormTypeEnum#VIEW}
+ * </pre>
+ * 
+ * @since 05/05/2014
+ * @author Guillermo Mazzali
+ * @version 1.0
+ */
+public enum FormTypeEnum {
+
+	/**
+	 * El tipo de formulario para el alta de una entidad.
+	 */
+	NEW,
+	/**
+	 * El tipo de formulario para la edición.
+	 */
+	EDIT,
+	/**
+	 * El tipo de formulario para la visualización de la entidad.
+	 */
+	VIEW;
+}
