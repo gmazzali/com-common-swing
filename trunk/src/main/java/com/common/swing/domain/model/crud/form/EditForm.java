eeca0492cfa3efdd8fb13453e63974245efbcdf1
guillermomazzali@gmail.com <guillermomazzali@gmail.com@e123b043-f167-5ba1-ef26-921036c7607d>
2014-05-06 19:41:41 +0000

    Cambios en el framework de crud.

--- trunk/src/main/java/com/common/swing/domain/model/crud/form/EditForm.java
+++ trunk/src/main/java/com/common/swing/domain/model/crud/form/EditForm.java
@@ -6,9 +6,7 @@ import com.common.swing.domain.model.crud.BaseForm;
 import com.common.swing.domain.model.crud.container.BaseContainer;
 import com.common.swing.domain.model.crud.util.CallbackForm;
 import com.common.swing.domain.model.crud.util.FormTypeEnum;
-import com.common.util.business.service.BaseService;
 import com.common.util.domain.exception.CheckedException;
-import com.common.util.domain.model.Persistence;
 
 /**
  * La interfaz que define los métodos para la ventana que va a representar un formulario de edición de entidades.
@@ -19,10 +17,8 @@ import com.common.util.domain.model.Persistence;
  * 
  * @param <E>
  *            La clase que establece las entidades que vamos a editar dentro de este formulario.
- * @param <PK>
- *            La clase que va a hacer de clave primaria de las entidades que vamos a editar.
  */
-public interface EditForm<E extends Persistence<PK>, PK extends Serializable> extends BaseForm {
+public interface EditForm<E extends Serializable> extends BaseForm {
 
 	/**
 	 * Se encarga de crear un formulario de acuerdo al tipo recibido y con la entidad que tenemos cargada dentro de esta.
@@ -35,13 +31,6 @@ public interface EditForm<E extends Persistence<PK>, PK extends Serializable> ex
 	 *            El contenedor donde vamos a colocar el formulario.
 	 */
 	public void createForm(BaseContainer container, FormTypeEnum formType, CallbackForm callback);
-
-	/**
-	 * Se encarga de retornar el servicio que vamos a utilizar para la entidad que tenemos en el panel.
-	 * 
-	 * @return El servicio para la entidad que tenemos en el panel.
-	 */
-	public BaseService<E, PK> getService();
 
 	/**
 	 * Permite recuperar la entidad que estamos manipulando dentro de este formulario, en caso de que el formulario se cancele, esto retornará
---
+++ trunk/src/main/java/com/common/swing/domain/model/crud/list/ListForm.java
@@ -0,0 +1,7 @@
+package com.common.swing.domain.model.crud.list;
+
+import com.common.swing.domain.model.crud.BaseForm;
+
+public interface ListForm extends BaseForm {
+
+}
